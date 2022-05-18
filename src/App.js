a6062bef2f591a809657ddf1079b435156458a95
amek1004 <choihantae@me.com>
2022-05-18 09:56:23 +0900

    master

--- src/App.js
+++ src/App.js
@@ -1,16 +1,7 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <nav>
-        <a>fdsfdfsdf</a>
-        <a>dfsafdfasdf</a>
-        <a>dfsadfasdfadsf</a>
-      </nav>
-    </div>
-  );
+  return <div className="appmenu">TOPMENU</div>;
 }
 
 export default App;
